2777dc4999f4e296668bf5318f0cbe99f8b79516
MartinEthier <methier@edu.uwaterloo.ca>
2019-01-22 22:37:55 -0500

    App init

---
+++ app/src/main/java/methier/edu/uwaterloo/ca/dogbreedapp/TakeImage.java
@@ -0,0 +1,13 @@
+package methier.edu.uwaterloo.ca.dogbreedapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TakeImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_image);
+    }
+}
